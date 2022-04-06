579aa8b6330b860e23af2fe452407573ef9d0063
Robert Cloa <8luM3CT0@users.noreply.github.com>
2022-04-05 23:38:13 -0700

    new commit: shit

--- components/index.js
+++ components/index.js
@@ -5,5 +5,6 @@ export {default as ModalBody} from '@material-tailwind/react/ModalBody'
 export {default as ModalFooter} from '@material-tailwind/react/ModalFooter'
 export {default as Button} from '@material-tailwind/react/Button'
 export {default as Icon} from '@material-tailwind/react/Icon'
+export {default as Input} from '@material-tailwind/react/Input'
 //local components
 export {default as Header} from './header/main-header/MainHeader'
---
+++ data/comments.js
@@ -0,0 +1,119 @@
+export const comments = [
+    {
+        id: 1,
+        word: '做愛',
+        pronounce: 'chò-ài',
+        wordType: 'verb',
+        meaning: 'make love',
+        email: 'reaperiff697@gmail.com',
+        name: 'Reaper IFF'
+    },
+    {
+        id: 2,
+        word: '樹根',
+        pronounce: 'chhīu-kin',
+        wordType: 'noun',
+        meaning: 'tree root',
+        email: 'rumlowb@gmail.com',
+        name: 'John Seed'
+    },
+    {
+        id: 3,
+        word: '回聲',
+        pronounce: 'hôe-siaⁿ',
+        wordType:'noun',
+        meaning: 'an echo',
+        email: 'reaperiff697@gmail.com',
+        name:'Reaper IFF'
+    },
+    {
+        id: 4,
+        word: '蝙蝠',
+        pronounce:'iā-pô (bi̍t-pô)',
+        wordType:'noun',
+        meaning: 'Bat',
+        email: 'reaperiff697@gmail.com',
+        name:'Reaper IFF'
+    },
+    {
+        id: 5,
+        word: '筆記型電腦',
+        pronounce: 'pit-kì-hêng tiān-náu',
+        wordType:'adjective',
+        meaning: 'Computer',
+        email: 'reaperiff697@gmail.com',
+        name:'Reaper IFF'
+    },
+    {
+        id: 6,
+        word: '將軍',
+        pronounce:'chiong-kun',
+        wordType: 'noun',
+        meaning: 'general',
+        email: 'rumlowb@gmail.com',
+        name: 'John Seed'
+    },
+    {
+        id: 7,
+        word: '精神作用',
+        pronounce: 'cheng-sîn chok-iōng',
+        wordType:'noun',
+        meaning:'psychological origin',
+        email: 'reaperiff697@gmail.com',
+        name: 'Reaper IFF'
+    },
+    {
+        id: 8,
+        word: '飛機',
+        pronounce: 'hui-ki',
+        wordType:'noun',
+        meaning:'airplane',
+        email: 'reaperiff697@gmail.com',
+        name: 'Reaper IFF'
+    },
+    {
+        id: 9,
+        word: '國王',
+        pronounce: 'kok-ông',
+        wordType:'noun',
+        meaning:'king',
+        email: 'reaperiff697@gmail.com',
+        name: 'Reaper IFF'
+    },
+    {
+        id: 10,
+        word: '寶劍',
+        pronounce: 'pó-kiàm',
+        wordType:'noun',
+        meaning:'magic sword',
+        email: 'reaperiff697@gmail.com',
+        name: 'Reaper IFF'
+    },
+    {
+        id: 11,
+        word: '房子',
+        pronounce: 'chhù',
+        wordType:'noun',
+        meaning:'house',
+        email: 'reaperiff697@gmail.com',
+        name: 'Reaper IFF'
+    },
+    {
+        id: 12,
+        word: '送回',
+        pronounce: 'sàng-hôe',
+        wordType:'verb',
+        meaning:'send back',
+        email: 'reaperiff697@gmail.com',
+        name: 'Reaper IFF'
+    },
+    {
+        id: 13,
+        word: '假裝',
+        pronounce: 'ké-kúi',
+        wordType:'verb',
+        meaning:'pretend',
+        email: 'reaperiff697@gmail.com',
+        name: 'Reaper IFF'
+    }
+]
