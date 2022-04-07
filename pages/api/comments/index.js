d2b483d778099f787496c7b2e439c8a0a02bf862
Robert Cloa <8luM3CT0@users.noreply.github.com>
2022-04-06 23:14:02 -0700

    new commit: for some stuff

--- pages/api/comments/index.js
+++ pages/api/comments/index.js
@@ -1,5 +1,21 @@
 import {comments} from '../../../data/comments'
 
 export default function handler(req, res) {
+    if(req.method === 'GET'){
     res.status(200).json(comments)
+    } else if (req.method === 'POST'){
+        const word = req.body.word
+        const wordType = req.body.wordType
+        const pronounce = req.body.pronounce
+        const meaning = req.body.meaning
+        const newWord = {
+            id: Date.now(),
+            word: word,
+            wordType: wordType,
+            pronounce: pronounce,
+            meaning: meaning
+        }
+        comments.push(newWord)
+        res.status(201).json(newWord)
+    }
 }
--- pages/index.js
+++ pages/index.js
@@ -14,14 +14,6 @@ import {
 import { useState } from 'react'
 
 export default function Home () {
-  const [comments, setComments] = useState([])
-
-  const fetchComments = async () => {
-    const response = await fetch('/api/comments')
-    const data = await response.json()
-    setComments(data)
-  }
-
   return (
     <>
       <div
@@ -45,8 +37,6 @@ export default function Home () {
         <main
           className='
       max-w-6xl 
-      overflow-y-scroll 
-      scrollbar-hide
       h-screen 
       bg-gray-300
       opacity-90 
@@ -55,38 +45,19 @@ export default function Home () {
       py-3
       '
         >
-          <div className='h-screen mx-auto max-w-2xl opacity-100'>
-            <Button
-              color='red'
-              buttonType='filled'
-              size='regular'
-              ripple='light'
-              onClick={fetchComments}
-            >
-              Fetch the words
-            </Button>
-            <div className='mx-auto max-w-2xl grid'>
-              {comments.map(comment => (
-                <div
-                  key={comment.id}
-                  className='
-            grid 
-            place-items-center 
-            space-y-4 
-            font-google-sans 
-            text-blue-400'
-                >
-                  <h2 className='text-2xl font-semibold'>
-                    {comment.id} - {comment.word}
-                  </h2>
-                  <h3 className='text-xl font-normal'>
-                    {comment.pronounce} - {comment.wordType}
-                  </h3>
-                  <h4 className='text-lg font-light'>{comment.meaning}</h4>
-                </div>
-              ))}
-            </div>
-          </div>
+          <div
+            className='
+          h-screen 
+          overflow-y-scroll 
+      scrollbar-hide
+          grid 
+          place-items-center 
+          mx-auto 
+          max-w-2xl 
+          opacity-100
+          pb-48
+          '
+          ></div>
         </main>
 
         <footer className='z-50 bottom-0 sticky bg-gray-600 px-8 py-4'>
